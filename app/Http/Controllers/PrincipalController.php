d1bf4664ca9d1ba939e5ef0c97f7c74013e3e0c1
CesarGuzmanLopez <88-8@live.com.mx>
2020-02-24 10:12:26 -0600

    parche

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Gate;
 
-class PrincipalController extends Controller 
+class PrincipalController extends Controller
+{
     /**
      * Create a new controller instance.
      *
